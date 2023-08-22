0fa554c6f4c0fe9061f2fa441a099ac1a86e5c22
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-22 20:06:41 +0630

    Add reqyured functions

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,2 +1,46 @@
-name = 'htet wai yan'
-puts name
+MORSE_CODE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(code)
+  MORSE_CODE[code]
+end
+
+def decode_word(word)
+  word = word.split.map { |char| decode_char(char) }
+  word.join
+end
+
+def decode(string)
+  sentence = string.split.map { |word| decode_word(word) }
+  sentence.join(' ')
+end
+
+decode_char('.-')
+decode_word('-- -.--')
+decode('-- -.--   -. .- -- .')
